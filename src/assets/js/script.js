209b50aa479e996b27ee9a66dd112ceb65577280
fjcappello <fjcappello@live.com>
2023-09-16 20:24:44 -0300

    Finalizado scripts

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,26 +1,53 @@
-// Obtén una referencia al botón "Ordenar" y agrega un evento click
+
+// Script para el carrito de pedidos
+
+
 const botonOrdenar = document.querySelector('.carrito');
 botonOrdenar.addEventListener('click', () => {
-  // Crea una lista de productos pedidos
+
   const productosPedidos = [];
 
-  // Obtén todos los botones "Pedir"
+
   const botonesPedir = document.querySelectorAll('.pedir');
 
-  // Recorre los botones y agrega los productos al carrito
+
   botonesPedir.forEach((boton) => {
-    const nombreProducto = boton.getAttribute('data-nombre');
-    productosPedidos.push(nombreProducto);
+
+    if (boton.classList.contains('agregado')) {
+      const nombreProducto = boton.getAttribute('data-nombre');
+      productosPedidos.push(nombreProducto);
+    }
   });
 
-  // Genera un nombre de mesero aleatorio (reemplace con sus nombres)
+
   const meseros = ["Juan", "Maria", "Carlos", "Laura", "Pedro", "Ana", "Diego", "Carmen"];
   const nombreMesero = meseros[Math.floor(Math.random() * meseros.length)];
 
-  // Crea un mensaje con los productos y el nombre del mesero
-  const mensaje = `Su orden:\n${productosPedidos.join('\n')}\n\nHa sido enviada al mesero.\nSu mesero hoy es ${nombreMesero}.`;
+  if (productosPedidos.length > 0) {
 
-  // Muestra la notificación
-  alert(mensaje);
+    const mensaje = `Su orden:\n${productosPedidos.join('\n')}\n\nHa sido enviada al mozo.\nSu mozo hoy es ${nombreMesero}.`;
+
+
+    alert(mensaje);
+
+
+    botonesPedir.forEach((boton) => {
+      boton.classList.remove('agregado');
+    });
+  } else {
+    alert('No ha agregado ningún producto al carrito.');
+  }
 });
 
+// Notificaciones de productos agregados a la orden
+
+const botonesPedir = document.querySelectorAll('.pedir');
+botonesPedir.forEach((boton) => {
+  boton.addEventListener('click', () => {
+
+    boton.classList.add('agregado');
+
+    const nombreProducto = boton.getAttribute('data-nombre');
+    alert(`Se ha agregado ${nombreProducto} al carrito.`);
+  });
+});
