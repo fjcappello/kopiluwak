d130d2ada72d3dc6e50cdcb6da3278dac231070e
fjcappello <fjcappello@live.com>
2023-10-13 10:26:34 -0300

    agregado manejo JS

---
+++ src/assets/js/contacto.js
@@ -0,0 +1,18 @@
+const formularioContacto = document.getElementById('formularioContacto');
+  
+formularioContacto.addEventListener('submit', function (e) {
+    e.preventDefault(); 
+    
+
+    const nombres = document.getElementById('nombres').value;
+    const correo = document.getElementById('correo').value;
+    const mensaje = document.getElementById('mensaje').value;
+
+
+    const popupMensaje = `Tu nombre: ${nombres}\nTu correo: ${correo}\nTu mensaje: ${mensaje}\n\n El mensaje fue enviado con exito! \n Proximamente nos comunicaremos con vos!`;
+
+    alert(popupMensaje);
+
+
+    formularioContacto.reset();
+});
